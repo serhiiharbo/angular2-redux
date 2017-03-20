b556311ff31335fdae4316915e28e6721ed3b16e
Serhii.Harbovskyi <serhii.harbovskyi@ihsmarkit.com>
2017-03-20 12:51:22 +0200

    changing styles for windows

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 
-import { DataResolver } from './app.resolver';
-
 export const ROUTES: Routes = [
   { path: '',  component: HomeComponent },
   { path: 'patient-info',  loadChildren: './+patient-info#PatientInfoModule'},
